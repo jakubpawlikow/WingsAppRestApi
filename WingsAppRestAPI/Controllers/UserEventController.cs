720d2fee0c17520a41018d59f8f8de48bce5aac2
j.pawlikow <jakub.pawlikow@gmail.com>
2019-06-19 22:35:00 +0200

    Prepare basic REST API

---
+++ WingsAppRestAPI/Controllers/UserEventController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WingsAppBLL;
+using WingsAppBLL.BusinessObjects;
+
+namespace WingsAppRestAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserEventController : ControllerBase
+    {
+        BLLFacade facade = new BLLFacade();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserEventBO>> Get()
+        {
+            return facade.UserEventService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<UserEventBO> Get(int id)
+        {
+            return facade.UserEventService.Get(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] UserEventBO user_event)
+        {
+            facade.UserEventService.Create(user_event);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] UserEventBO user_event)
+        {
+            facade.UserEventService.Update(user_event);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            facade.UserEventService.Delete(id);
+        }
+    }
+}
